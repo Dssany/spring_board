c70722843a720a64eccea5aecb6433b3308a6a95
bin <tjdqls45685210@gmail.com>
2020-06-23 09:10:59 +0900

    rebase runtime setting commit

--- src/main/java/com/care/boardService/BoardService.java
+++ src/main/java/com/care/boardService/BoardService.java
@@ -1,7 +1,8 @@
 package com.care.boardService;
 
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
-
+@Service
 public interface BoardService {
 	public void execute(Model model);
 }
